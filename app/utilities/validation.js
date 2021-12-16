a510fb299eb9c09c62b82e611cb97af2db971d1f
RohitGupta8 <therohitg@gmail.com>
2021-12-16 14:47:04 +0530

    Refactor: code.

--- app/utilities/validation.js
+++ app/utilities/validation.js
@@ -3,32 +3,23 @@ const Joi = require("joi");
 class Validation {
     validDetails =
         Joi.object({
-          firstName: Joi.string().min(3).max(30).required()
-            .pattern(new RegExp("^([A-Z]?[a-zA-Z]{1,30}[ ]?[.]?[]?[] ? [a - zA - Z]{ 1, 30}[] ? [.] ? []?[ ]?[a-zA-Z]{0,30}[ ]?[a-zA-Z]{0,30}?)")),
+            firstName: Joi.string().min(3).max(20).required()
+                .pattern(new RegExp("([A-Z][a-z]*)([\\s\\\'-][A-Z][a-z]*)*")),
 
-          lastName: Joi.string().min(2).max(30)
-            .required()
-            .pattern(new RegExp("^([A-Z]?[a-zA-Z]{1,30}[ ]?[.]?[]?[] ? [a - zA - Z]{ 1, 30}[] ? [.] ? []?[ ]?[a-zA-Z]{0,30}[ ]?[a-zA-Z]{0,30}?)")),
+            lastName: Joi.string().min(2).max(20).required()
+                .pattern(new RegExp("([A-Z][a-z]*)([\\s\\\'-][A-Z][a-z]*)*")),
 
-          email: Joi.string()
-            .pattern(new RegExp("^[a-zA-z]{3}([+-_ .]*[a-zA-Z0-9]+)*[@][a-zA-z0-9]+(.[a-z]{2,3})*$"))
-            .required(),
+            email: Joi.string()
+                .pattern(new RegExp("^^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$")).required(),
 
-          password: Joi.string()
-            .pattern(new RegExp("(?=^.{8,}$)((?=.*\\d)|(?=.*\\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$"))
-            .required()
+            password: Joi.string()
+                .pattern(new RegExp("(?=^.{8,}$)((?=.*\\d)|(?=.*\\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$")).required()
         });
 
     validLogin =
         Joi.object({
-          email: Joi.string()
-            .pattern(new RegExp("^[a-zA-z]{3}([+-_ .]*[a-zA-Z0-9]+)*[@][a-zA-z0-9]+(.[a-z]{2,3})*$"))
-            .required(),
-
-          password: Joi.string()
-            .required()
-            .pattern(new RegExp("(?=^.{8,}$)((?=.*\\d)|(?=.*\\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$"))
+            email: Joi.string().pattern(new RegExp("^^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$")).required(),
+            password: Joi.string().required().pattern(new RegExp("(?=^.{8,}$)((?=.*\\d)|(?=.*\\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$"))
         });
 }
-
 module.exports = new Validation();
