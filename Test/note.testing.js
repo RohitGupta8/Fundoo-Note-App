a1bfcf3bd5d910dc9e5543c00d89530acfa16148
RohitGupta8 <therohitg@gmail.com>
2021-12-26 18:30:38 +0530

    Feat: check response with valid and invalid token from Controller.

--- Test/note.testing.js
+++ Test/note.testing.js
@@ -192,4 +192,20 @@ describe("Get all Notes", () => {
         return done();
       });
   });
+
+  it.only("when call getNote api with token, should return appropriate response from controller", (done) => {
+    const token = noteData.notes.inValidToken;
+    chai
+      .request(server)
+      .get("/getNote")
+      .set({ authorization: token })
+      .end((err, res) => {
+        if (err) {
+          console.log("plz check your credential");
+          return done();
+        }
+        res.should.have.status(400);
+        return done();
+      });
+  });
 });
