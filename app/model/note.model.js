020366c3ad974620954e6a035806b6b9391dd65c
RohitGupta8 <therohitg@gmail.com>
2021-12-26 13:59:20 +0530

    Feat: Add Collection & store into DB on model layer.

--- app/model/note.model.js
+++ app/model/note.model.js
@@ -1,8 +1,35 @@
+const mongoose = require("mongoose");
+const noteSchema = mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  title: {
+    type: String,
+    required: true,
+    minlength: 2
+  },
+  description: {
+    type: String,
+    required: true,
+    minlength: 2
+  }
+}, {
+  timestamps: true
+});
+const NoteRegister = mongoose.model("NoteBook", noteSchema);
+
 class NoteModel {
     createNote = (note, callback) => {
-      if (note) {
-        callback(null, note);
-      }
+      const notes = new NoteRegister({
+        userId: note.userId,
+        title: note.title,
+        description: note.description
+      });
+      notes.save((error, data) => {
+        if (error) {
+          return callback(error, null);
+        } else {
+          return callback(null, data);
+        }
+      });
     }
 }
 module.exports = new NoteModel();
