d668c73e128ad054b7ae47f0cd19b8d8094c1178
RohitGupta8 <therohitg@gmail.com>
2021-12-27 15:00:38 +0530

    Feat: using TDD , check validation of valid input.

--- Test/note.testing.js
+++ Test/note.testing.js
@@ -439,7 +439,7 @@ describe("Update Note By Id", () => {
       });
   });
 
-  it.only("when call updateNoteById with inValidToken , should return appropriate response from controller", (done) => {
+  it("when call updateNoteById with inValidToken , should return appropriate response from controller", (done) => {
     const token = noteData.notes.inValidToken;
     chai
       .request(server)
@@ -451,6 +451,23 @@ describe("Update Note By Id", () => {
           return done();
         }
         res.should.have.status(400);
+        return done();
+      });
+  });
+
+  it.only("when call updateNoteById with valid input , should return appropriate response from controller", (done) => {
+    const token = noteData.notes.validToken;
+    chai
+      .request(server)
+      .put("/updateNote/61c8913bdcf696ac219ce3ea")
+      .set({ authorization: token })
+      .send({ title: "School", description: "golden period of life" })
+      .end((err, res) => {
+        if (err) {
+          console.log("plz check your credential");
+          return done();
+        }
+        res.should.have.status(201);
         return done();
       });
   });
