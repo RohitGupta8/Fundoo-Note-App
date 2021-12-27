5e0438c567241bac036661a666caa2ee7c537fce
RohitGupta8 <therohitg@gmail.com>
2021-12-27 16:57:11 +0530

    Feat: using TDD,check given id is present in DB then deleted.

--- app/model/note.model.js
+++ app/model/note.model.js
@@ -75,7 +75,12 @@ class NoteModel {
   }
 
   deleteNoteById = (id, callback) => {
-    return callback(null, id);
-  };
+    NoteRegister.findOneAndDelete({ $and: [{ _id: id.noteId }, { userId: id.userId }] }, (error, data) => {
+      if (data) {
+        return callback(null, data);
+      }
+      return callback(error, null);
+    });
+  }
 }
 module.exports = new NoteModel();
