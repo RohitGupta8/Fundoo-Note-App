23ab50bbd754adad45dcc590f6c3fec0bdb20523
RohitGupta8 <therohitg@gmail.com>
2021-12-27 16:17:39 +0530

    Feat: using Tdd,check response with inValid token from controller.

--- Test/note.testing.js
+++ Test/note.testing.js
@@ -578,7 +578,7 @@ describe("Update Note By Id", () => {
 // api of Delete Note by Id
 
 describe("DeleteNoteById", () => {
-  it.only("when call DeleteNote Api with valid token, should return proper response from controller", (done) => {
+  it("when call DeleteNote Api with valid token, should return proper response from controller", (done) => {
     const token = noteData.notes.validToken;
     chai
       .request(server)
@@ -590,6 +590,22 @@ describe("DeleteNoteById", () => {
           return done();
         }
         res.should.have.status(201);
+        return done();
+      });
+  });
+
+  it.only("when call DeleteNote Api with inValid token, should return proper response from controller", (done) => {
+    const token = noteData.notes.inValidToken;
+    chai
+      .request(server)
+      .delete("/deleteNote/:id")
+      .set({ authorization: token })
+      .end((err, res) => {
+        if (err) {
+          console.log("plz check your credential");
+          return done();
+        }
+        res.should.have.status(400);
         return done();
       });
   });
