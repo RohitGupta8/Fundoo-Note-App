069eb4a69f1d73d3a5ce9e5e1df9510cd228fe6f
RohitGupta8 <therohitg@gmail.com>
2021-12-27 15:02:29 +0530

    Feat: using TDD , check validation of false title.

--- Test/note.testing.js
+++ Test/note.testing.js
@@ -455,7 +455,7 @@ describe("Update Note By Id", () => {
       });
   });
 
-  it.only("when call updateNoteById with valid input , should return appropriate response from controller", (done) => {
+  it("when call updateNoteById with valid input , should return appropriate response from controller", (done) => {
     const token = noteData.notes.validToken;
     chai
       .request(server)
@@ -468,6 +468,23 @@ describe("Update Note By Id", () => {
           return done();
         }
         res.should.have.status(201);
+        return done();
+      });
+  });
+
+  it.only("when call updateNoteById with false title , should return appropriate response from controller", (done) => {
+    const token = noteData.notes.validToken;
+    chai
+      .request(server)
+      .put("/updateNote/61c8913bdcf696ac219ce3ea")
+      .set({ authorization: token })
+      .send({ title: "S", description: "golden period of life" })
+      .end((err, res) => {
+        if (err) {
+          console.log("plz check your credential");
+          return done();
+        }
+        res.should.have.status(400);
         return done();
       });
   });
