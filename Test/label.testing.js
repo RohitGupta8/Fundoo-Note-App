7cff95fbd9eadf5fb67f09cd24cb18a9e902466b
RohitGupta8 <therohitg@gmail.com>
2021-12-28 12:04:05 +0530

    Feat: using TDD ,check response with invalid token from Controller.

--- Test/label.testing.js
+++ Test/label.testing.js
@@ -6,7 +6,7 @@ chai.should();
 const labelData = require("./label.token.json");
 
 describe("Add Label", () => {
-  it.only("when call AddLabel api, should return appropriate response from controller", (done) => {
+  it("when call AddLabel api, should return appropriate response from controller", (done) => {
     const token = labelData.notes.validToken;
     chai
       .request(server)
@@ -19,6 +19,23 @@ describe("Add Label", () => {
           return done();
         }
         res.should.have.status(201);
+        return done();
+      });
+  });
+
+  it.only("when call AddLabel api with invalid token, should return appropriate response from controller", (done) => {
+    const token = labelData.notes.inValidToken;
+    chai
+      .request(server)
+      .post("/addLabel")
+      .set({ authorization: token })
+      .send({})
+      .end((err, res) => {
+        if (err) {
+          console.log("plz check your credential");
+          return done();
+        }
+        res.should.have.status(400);
         return done();
       });
   });
