671cc1ba54e192b3f675f3328b6fa32c001e1f31
RohitGupta8 <therohitg@gmail.com>
2022-01-07 16:05:49 +0530

    Feat: using TDD, check data comes from DB not from Redis.

--- Test/note.testing.js
+++ Test/note.testing.js
@@ -428,7 +428,7 @@ describe("GetNoteById", () => {
       });
   });
 
-  it.only("check data fetch from Redis with false param, should return code = 500", (done) => {
+  it("check data fetch from Redis with false param, should return code = 500", (done) => {
     const token = noteData.notes.validToken;
     chai
       .request(server)
@@ -440,6 +440,22 @@ describe("GetNoteById", () => {
           return done();
         }
         res.should.have.status(500);
+        return done();
+      });
+  });
+
+  it("check data from DB not from Redis, should return code = 500", (done) => {
+    const token = noteData.notes.validToken;
+    chai
+      .request(server)
+      .get("/note/61d696606632cae8f42d9b49")
+      .set({ authorization: token })
+      .end((err, res) => {
+        if (err) {
+          console.log("plz check your credential");
+          return done();
+        }
+        res.should.have.status(201);
         return done();
       });
   });
