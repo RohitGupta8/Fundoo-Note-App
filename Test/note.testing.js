c136465d11aa63ff3274802914d7f07f17a70300
RohitGupta8 <therohitg@gmail.com>
2022-01-07 22:07:11 +0530

    feat: using TDD,check data from DB after clear cache.

--- Test/note.testing.js
+++ Test/note.testing.js
@@ -490,6 +490,22 @@ describe("GetNoteById", () => {
         return done();
       });
   });
+
+  it("check data from DB after delete cache, should return code = 201", (done) => {
+    const token = noteData.notes.validToken;
+    chai
+      .request(server)
+      .get("/note/61d696606632cae8f42d9b49")
+      .set({ authorization: token })
+      .end((err, res) => {
+        if (err) {
+          console.log("plz chec your credential");
+          return done();
+        }
+        res.should.have.status(201);
+        return done();
+      });
+  });
 });
 
 // api for update note by id
